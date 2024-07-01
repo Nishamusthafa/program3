4127aa84f9d9e839036f1e2ae1a702dfcb7cbff6
Nishamusthafa <23203019@srcas.ac.in>
2024-07-01 11:58:00 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,9 @@
+class sample{
+  public static void main(String[]args)
+  {
+    system.out.println("Hello");
+    system.out.println("Welcome");
+    system.out.println("to");
+    system.out.println("Devops");
+  }
+}
